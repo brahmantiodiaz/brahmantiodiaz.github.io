4262ad320856894386423d64419a81c4fc051b36
brahmantiodiaz28 <brahmantiodiaz28@gmail.com>
2022-10-05 13:52:43 +0700

    add user flow

--- script/equine-cookies.js
+++ script/equine-cookies.js
@@ -1,21 +1,22 @@
-console.log(visitData.get());
-// console.log(document.cookie);
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-console.log(getCookie("_ga_6QBT1DNHZ6"));
-let tmpID = getCookie("_ga_6QBT1DNHZ6").split(".");
-let ga_session_id = tmpID[2];
-console.log(ga_session_id);
+// console.log(visitData.get());
+// // let options = { year: "numeric", month: "numeric", day: "numeric" };
+// // console.log(document.cookie);
+// function getCookie(cname) {
+//   let name = cname + "=";
+//   let decodedCookie = decodeURIComponent(document.cookie);
+//   let ca = decodedCookie.split(";");
+//   for (let i = 0; i < ca.length; i++) {
+//     let c = ca[i];
+//     while (c.charAt(0) == " ") {
+//       c = c.substring(1);
+//     }
+//     if (c.indexOf(name) == 0) {
+//       return c.substring(name.length, c.length);
+//     }
+//   }
+//   return "";
+// }
+// console.log(getCookie("_ga_6QBT1DNHZ6"));
+// let tmpID = getCookie("_ga_6QBT1DNHZ6").split(".");
+// let ga_session_id = tmpID[2];
+// console.log(ga_session_id);
