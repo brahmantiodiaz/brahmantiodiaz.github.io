fcb2950ae1bb63eca77814a33bca3d0975737cab
brahmantiodiaz28 <brahmantiodiaz28@gmail.com>
2022-12-08 16:12:36 +0700

    Update npp-tracking.js

--- script/npp-tracking.js
+++ script/npp-tracking.js
@@ -26,7 +26,22 @@ window.addEventListener("load", (event) => {
   if (!FRUBIL.device.marketname) {
     fullDeviceName = FRUBIL.client.os + " using " + FRUBIL.client.name;
   }
-
+  //getcookie funtion
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
   //getIP
   async function getIp() {
     const result = await axios
