1b8e7d802b3e210eafeff4abe4e58ca7b3a21dba
brahmantiodiaz28 <brahmantiodiaz28@gmail.com>
2022-12-20 14:36:26 +0700

    Update npp-tracking.js

--- script/npp-tracking.js
+++ script/npp-tracking.js
@@ -150,7 +150,7 @@ window.addEventListener("load", (event) => {
       name: name.value,
       email: email.value,
       company: company.value,
-      phone: phone.value,
+      phone: '"' + phone.value + '"',
       date: date.toLocaleDateString("en-US", options),
       ip: dataIP.ip,
       country: dataIP.country,
